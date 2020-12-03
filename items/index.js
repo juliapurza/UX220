2fc2e93c85597b598a304d8b62c0fc6100c390c5
juliapurza <75149407+juliapurza@users.noreply.github.com>
2020-12-03 10:38:07 -0500

    Add files via upload

---
+++ items/index.js
@@ -0,0 +1,5 @@
+export default [{"id": "Horseback", "fname":"Horseback.md", "specialImage":"/img/Horseback.jpg"},
+{"id": "Romania", "fname":"Romania.md", "specialImage":"/img/Romania.jpg"},
+{"id": "Volunteer", "fname":"Volunteer.md", "specialImage":"/img/Volunteer.jpg"},
+{"id": "Artshow", "fname":"Artshow.md", "specialImage":"/img/Artshow.jpg"},
+{"id": "Shoppers", "fname":"Shoppers.md", "specialImage":"/img/Shoppers.jpg"}];
