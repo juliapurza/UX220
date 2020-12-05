82dc7f0f4d75835325fc6dff62ac67a91fbb296f
Julia Purza <juliapurza@Julias-MacBook-Air-2.local>
2020-12-05 11:43:10 -0500

    change copyright

--- items/index.js
+++ items/index.js
@@ -1,5 +1,5 @@
-export default [{"id": "UX201", "fname":"UX201.md", "pdf":"/img/PAMIGA.pdf"},
-{"id": "UX230", "fname":"UX230.md", "pdf":"/img/Romania.jpg"},
+export default [{"id": "UX201", "fname":"UX201.md", "pdf":"/img/UX201.jpg"},
+{"id": "UX230", "fname":"UX230.md", "pdf":"/img/.jpg"},
 {"id": "Volunteer", "fname":"Volunteer.md", "pdf":"/img/Volunteer.jpg"},
 {"id": "Artshow", "fname":"Artshow.md", "pdf":"/img/Artshow.jpg"},
 {"id": "Shoppers", "fname":"Shoppers.md", "pdf":"/img/Shoppers.jpg"}];
