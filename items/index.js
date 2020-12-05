5b7b25b09e61b501854c4bc43e927abe43f2efec
Julia Purza <juliapurza@Julias-MacBook-Air-2.local>
2020-12-05 01:52:09 -0500

    change copyright

--- items/index.js
+++ items/index.js
@@ -1,5 +1,5 @@
-export default [{"id": "Horseback", "fname":"Horseback.md", "specialImage":"/img/Horseback.jpg"},
-{"id": "Romania", "fname":"Romania.md", "specialImage":"/img/Romania.jpg"},
-{"id": "Volunteer", "fname":"Volunteer.md", "specialImage":"/img/Volunteer.jpg"},
-{"id": "Artshow", "fname":"Artshow.md", "specialImage":"/img/Artshow.jpg"},
-{"id": "Shoppers", "fname":"Shoppers.md", "specialImage":"/img/Shoppers.jpg"}];
+export default [{"id": "UX201", "fname":"UX201.md", "pdf":"/img/PAMIGA.pdf"},
+{"id": "UX230", "fname":"UX230.md", "pdf":"/img/Romania.jpg"},
+{"id": "Volunteer", "fname":"Volunteer.md", "pdf":"/img/Volunteer.jpg"},
+{"id": "Artshow", "fname":"Artshow.md", "pdf":"/img/Artshow.jpg"},
+{"id": "Shoppers", "fname":"Shoppers.md", "pdf":"/img/Shoppers.jpg"}];
